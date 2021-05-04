5586e4ecfae1300874658205b58881c441503c28
VidunderGunder <kristiankramas@outlook.com>
2021-05-04 23:29:58 +0200

    CSS Prop type declaration attempt

---
+++ libs/components/styled.d.ts
@@ -0,0 +1,8 @@
+import { DOMAttributes } from "react";
+import { CSSProp } from "styled-components";
+
+declare module "react" {
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    css?: CSSProp;
+  }
+}
